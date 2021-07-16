06a9a450c039fa8bd68377cadc72ef3c4d040ce8
Souheil Chelfouh <trollfot@gmail.com>
2021-07-16 15:23:32 +0200

    first commit

---
+++ setup.py
@@ -0,0 +1,41 @@
+import os
+from setuptools import setup, find_packages
+
+
+version = "0.1"
+
+install_requires = [
+]
+
+test_requires = [
+    'pytest'
+]
+
+
+setup(
+    name='roughrider.cors',
+    version=version,
+    author='Souheil CHELFOUH',
+    author_email='trollfot@gmail.com',
+    url='',
+    download_url='http://pypi.python.org/pypi/roughrider.cors',
+    description='CORS Policy for Web applications',
+    long_description=(open("README.rst").read() + "\n" +
+                      open(os.path.join("docs", "HISTORY.rst")).read()),
+    license='ZPL',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Zope Public License',
+        'Programming Language :: Python:: 3 :: Only',
+    ],
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    namespace_packages=['roughrider',],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=install_requires,
+    extras_require={
+        'test': test_requires,
+    },
+)
---
+++ src/roughrider/cors/policy.py
@@ -0,0 +1,55 @@
+from typing import Optional, Set, NamedTuple, Literal, Tuple
+
+
+HEADER = Tuple[str, str]
+HEADERS = Iterator[HEADER]
+HTTPVerb = Literal[
+    "GET", "HEAD", "PUT", "DELETE", "PATCH", "POST", "OPTIONS"]
+
+
+class CORSPolicy(NamedTuple):
+    origin: str = "*"
+    methods: Optional[Set[HTTPVerb]] = None
+    allow_headers: Optional[Set[str]] = None
+    expose_headers: Optional[Set[str]] = None
+    credentials: Optional[bool] = None
+    max_age: Optional[int] = None
+
+    def headers(self) -> Headers:
+        yield "Access-Control-Allow-Origin", self.origin
+        if self.allow_headers:
+            values = ", ".join(self.methods)
+            yield "Access-Control-Allow-Methods", values
+        if self.allow_headers:
+            values = ", ".join(self.allow_headers)
+            yield "Access-Control-Allow-Headers", values
+        if self.expose_headers:
+            values = ", ".join(self.expose_headers)
+            yield "Access-Control-Expose-Headers", values
+        if self.credentials:
+            yield "Access-Control-Allow-Credentials", "true"
+        if self.max_age:
+            yield "Access-Control-Max-Age", str(self.max_age)
+
+    def preflight(self, environ) -> Headers:
+        if origin := environ.get("HTTP_ORIGIN"):
+            if self.origin == '*':
+                yield "Access-Control-Allow-Origin", '*'
+            elif origin == self.origin:
+                yield "Access-Control-Allow-Origin", origin
+                yield "Vary", "Origin"
+
+        if not self.methods:
+            if method := environ.get("HTTP_ACCESS_CONTROL_REQUEST_METHOD"):
+                yield "Access-Control-Allow-Methods", method
+        else:
+            yield "Access-Control-Allow-Methods", ", ".join(self.methods)
+
+        if self.allow_headers:
+            values = ", ".join(self.allow_headers)
+            yield "Access-Control-Allow-Headers", values
+        elif self.expose_headers:
+            values = ", ".join(self.expose_headers)
+            yield "Access-Control-Expose-Headers", values
+        elif acrh := environ.get("HTTP_ACCESS_CONTROL_REQUEST_HEADERS"):
+            yield "Access-Control-Allow-Methods", acrh
